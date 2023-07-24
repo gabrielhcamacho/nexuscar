d2e965878783114fb612baa489a6efbff8fc5eb7
Gabriel Camacho <gabriel.hcamacho@outlook.com>
2023-07-24 17:40:21 -0300

    erro de validação tratado

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,8 +1,9 @@
 import React, { createContext, useState, useContext, ReactNode } from 'react';
 
-import {api} from '../services/api';
+import { api } from '../services/api';
+import { Alert } from 'react-native';
 
-interface User{
+interface User {
     id: string,
     email: string,
     name: string,
@@ -10,54 +11,66 @@ interface User{
     avatar: string,
 }
 
-interface AuthState{
+interface AuthState {
     token: string,
     user: User,
 }
 
-interface SignInCredentials{
+interface SignInCredentials {
     email: string,
     password: string
 }
 
-interface AuthContextData{
+interface AuthContextData {
     user: User;
     signIn: (credentials: SignInCredentials) => Promise<void>;
 }
 
-interface AuthProviderProps{
+interface AuthProviderProps {
     children: ReactNode;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
-function AuthProvider({ children }: AuthProviderProps){
+function AuthProvider({ children }: AuthProviderProps) {
     const [data, setData] = useState<AuthState>({} as AuthState)
 
-    async function signIn({email, password}: SignInCredentials){
-        const response = await api.post('/sessions', {
-            email, 
-            password
-        })
+    async function signIn({ email, password }: SignInCredentials) {
+        try {
+            const response = await api.post('/sessions', {
+                email,
+                password,
+            });
 
-        const { token, user } = response.data
-        api.defaults.headers.authorization = `Bearer ${token}`
-        setData({token, user})
+            const { token, user } = response.data;
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({ token, user });
+        } catch (error) {
+            const apiError = error as { response?: { status: number } };
+
+            if (apiError.response && apiError.response.status === 401) {
+                // Handle invalid credentials, e.g., display an error message to the user
+                Alert.alert('Erro na autenticação', 'Credenciais invalidas. Por favor verifique o email e senha e tente novamente');
+            } else {
+                // Handle other errors, e.g., display a generic error message
+                Alert.alert('Erro na autenticação', 'Credenciais invalidas. Por favor verifique o email e senha e tente novamente');
+            }
+        }
     }
 
-    return(
+    return (
         <AuthContext.Provider
             value={{ user: data.user, signIn }}
         >
-            { children }
+            {children}
         </AuthContext.Provider>
     )
 }
 
-function useAuth(): AuthContextData{
+function useAuth(): AuthContextData {
     const context = useContext(AuthContext)
 
-    return context 
+    return context
 }
 
 export { AuthProvider, useAuth }
